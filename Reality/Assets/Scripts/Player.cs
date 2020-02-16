18c2726cc4719fb99f828afe2debf99cdc1f9368
spyro21 <38793743+spyro21@users.noreply.github.com>
2020-02-16 10:02:41 -0600

    darkmode start

--- Reality/Assets/Scripts/Player.cs
+++ Reality/Assets/Scripts/Player.cs
@@ -6,24 +6,41 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
 
+
     private Vector3 movement;
     public float speed;
+    public bool darkMode;
 
-    // Start is called before the first frame update
+
     void Start()
     {
+        darkMode = false;
         rb = this.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+
+       // if (Input.getAxisRaw) {
+        //}
+
+        if (darkMode) {
+            this.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+
+        if (!darkMode) {
+            this.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+
+        // movement
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate()
     {
+
+        // movement
         rb.MovePosition(transform.position + movement * speed * Time.fixedDeltaTime);
     }
 }
